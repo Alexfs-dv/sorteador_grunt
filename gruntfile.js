3eb8678bb7d060b704fa998c13ed79b2c5214ab8
Alex Faustino <alexfaustinosouza@gmail.com>
2023-11-28 23:42:42 -0300

    commit após aula htmlmin

--- gruntfile.js
+++ gruntfile.js
@@ -20,6 +20,10 @@ module.exports = function(grunt){
             less: {
                 files: ['src/styles/**/*.less'],
                 tasks: ['less:development']
+            },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
             }
         },
         replace: {
@@ -40,10 +44,28 @@ module.exports = function(grunt){
                         dest: 'dev/'
                     }
                 ]
+            },
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
             }
         },
         htmlmin: {
-            dit: {
+            dist: {
                 options: {
                     removeComments: true,
                     collapseWhitespace: true
@@ -52,14 +74,16 @@ module.exports = function(grunt){
                     'prebuild/index.html':'src/index.html'
                 }
             }
-        }
+        },
+        clean: ['prebuild']
     })
 
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     
     grunt.registerTask('default',['watch']);
-    grunt.registerTask('build',['less:production']);
+    grunt.registerTask('build',['less:production','htmlmin:dist','replace:dist','clean']);
 }
